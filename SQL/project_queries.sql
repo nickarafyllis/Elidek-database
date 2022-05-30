a1072fd8cc4f4a6a1d6a3396eeef321b0953a11b
nickarafyllis <nickarafyllis@gmail.com>
2022-05-31 00:58:17 +0300

    3.6 3.7

---
+++ SQL/project_queries.sql
@@ -0,0 +1,24 @@
+#3.6
+
+select  
+concat(researcher.first_name,' ',researcher.last_name) as full_name, count(*)
+FROM researcher
+inner join works ON works.researcher_id=researcher.researcher_id
+inner join project
+on works.project_id=project.project_id
+where(timestampdiff(year, date_of_birth,current_date())<40 
+AND current_date()<project.enddate AND current_date()>project.startdate)
+group by researcher.researcher_id
+order by count(*) DESC
+
+#3.7
+
+SELECT executive_name, organisation_name, SUM(amount)
+FROM executive
+JOIN project 
+ON executive.executive_id=project.executive_id
+JOIN organisation
+ON project.organisation_id=organisation.organisation_id
+GROUP BY executive_name 
+ORDER BY SUM(amount) DESC
+LIMIT 5;
