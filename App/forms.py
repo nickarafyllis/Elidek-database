da68574f5f1f75aef834fb1e1d17caf4cb26b735
Sim1nis <ignatisignacio@gmail.com>
2022-06-03 15:50:37 +0300

    fcgg

--- App/forms.py
+++ App/forms.py
@@ -4,9 +4,17 @@ from wtforms.validators import DataRequired, Email
 
 ## when passed as a parameter to a template, an object of this class will be rendered as a regular HTML form
 ## with the additional restrictions specified for each field
-class StudentForm(FlaskForm):
-    name = StringField(label = "Name", validators = [DataRequired(message = "Name is a required field.")])
+class ResearcherForm(FlaskForm):
+    first_name = StringField(label = "First Name", validators = [DataRequired(message = "First name is a required field.")])
 
-    surname = StringField(label = "Surname", validators = [DataRequired(message = "Surname is a required field.")])
+    last_name = StringField(label = "Last Name", validators = [DataRequired(message = "Last name is a required field.")])
+
+    sex = StringField(label = "Sex", validators = [DataRequired(message = "Sex is a required field.")])
+
+    date_of_birth = StringField(label = "Date Of Birth", validators = [DataRequired(message = "Date of birth is a required field.")])
+
+    employment_date = StringField(label = "Employment Date", validators = [DataRequired(message = "Employment date is a required field.")])
+
+    organisation_name = StringField(label = "Organisation Name", validators = [DataRequired(message = "Organisation name is a required field.")])
 
     submit = SubmitField("Create")
