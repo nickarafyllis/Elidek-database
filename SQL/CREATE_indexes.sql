f7ac1ddf7d9ca708c72677e0dae20ef7528f09b7
tasossaf <tasossaf01@gmail.com>
2022-06-03 04:31:27 +0300

    index

---
+++ SQL/CREATE_indexes.sql
@@ -0,0 +1,5 @@
+CREATE INDEX idx_Startdate ON project(startdate)
+CREATE INDEX idx_Enddate ON project(enddate)
+CREATE INDEX idx_resfirstname ON researcher(first_name)
+CREATE INDEX idx_reslastname ON researcher(last_name)
+CREATE INDEX idx_worktitle ON works(title)
