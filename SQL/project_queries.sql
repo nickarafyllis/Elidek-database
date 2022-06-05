a2cfe1e1a4f619e5749b962cb68b76f33653f939
tasossaf <tasossaf01@gmail.com>
2022-06-05 20:49:24 +0300

    llj

--- SQL/project_queries.sql
+++ SQL/project_queries.sql
@@ -59,6 +59,26 @@ join project
 on project.title=has.title
 where(current_date()<enddate AND current_date()>startdate AND timestampdiff(year,startdate, current_date())<1 AND has.scientific_field_name='Nuclear physics');
 
+#3.4  -- View
+CREATE VIEW
+projperyear (organisation_name, project, year)
+as 
+select a.organisation_name, count(*), year(b.assessment_date) as year
+from organisation a 
+inner join project p 
+on a.organisation_name = p.organisation_name
+inner join assessment b 
+on p.assessment_id = b.assessment_id
+group by a.organisation_name, year 
+having count(*)>9
+
+#3.4 -- query
+select d.organisation_name, d.project, d.year, e.year
+from projperyear d
+inner join projperyear e 
+on d.organisation_name=e.organisation_name
+where d.year=e.year-1
+
 #3.5 
 SELECT A.scientific_field_name AS scientificfield1, B.scientific_field_name AS scientificfield2, count(*) as count
 FROM has A, has B
